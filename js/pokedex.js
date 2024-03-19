53895b2095a091dd20b51ab9636319d7e3f25ba5
Taciany Maria S <138811627+tacianyalmeida@users.noreply.github.com>
2024-03-18 22:35:00 -0300

    Update pokedex.js

--- js/pokedex.js
+++ js/pokedex.js
@@ -50,7 +50,7 @@ return imgs;
 
 const getPokemons = async ()=>{
  try{ 
-const response =await fetch('https://pokeapi.co/api/v2/pokemon?limit=150&offset=0')
+const response =await fetch('https://pokeapi.co/api/v2/pokemon?limit=100&offset=0')
 if(!response.ok){
   throw new Error('Não foi possivel obter as informações')
 }
